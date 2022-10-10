f84885e9b677a21d5e815983d3578ffe34065e45
ssalpyo <tjdvy6@gmail.com>
2022-10-10 22:05:39 +0900

    # Fix: 배너 UI 수정

--- src/components/commons/layout/banner/LayoutBanner.presenter.tsx
+++ src/components/commons/layout/banner/LayoutBanner.presenter.tsx
@@ -1,24 +1,11 @@
-import { SliderItem, Wrapper, StyledSlider, ArrowImage } from "./LayoutBanner.styles";
-import styled from '@emotion/styled';
-
-const Next = styled.div`
-  width: 30px;
-  height: 30px;
-  position: absolute;
-  right: 30px;
-  z-index: 2;
-  text-align: right;
-  line-height: 30px;
-`;
-const Prev = styled.div`
-  width: 30px;
-  height: 30px;
-  position: absolute;
-  left: 30px;
-  z-index: 2;
-  text-align: left;
-  line-height: 30px;
-`;
+import {
+  SliderItem,
+  Wrapper,
+  StyledSlider,
+  ArrowImage,
+  Next,
+  Prev,
+} from "./LayoutBanner.styles";
 
 export default function LayoutBannerUI() {
   const settings = {
@@ -29,14 +16,14 @@ export default function LayoutBannerUI() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-		nextArrow: (
+    nextArrow: (
       <Next>
         <ArrowImage src="/images/next.png" alt="Next" />
       </Next>
     ),
     prevArrow: (
-			<Prev>
-				<ArrowImage src="/images/prev.png" alt="Prev" />
+      <Prev>
+        <ArrowImage src="/images/prev.png" alt="Prev" />
       </Prev>
     ),
   };
@@ -45,11 +32,8 @@ export default function LayoutBannerUI() {
     <Wrapper>
       <StyledSlider {...settings}>
         <SliderItem src="/images/banner1.png" />
-
         <SliderItem src="/images/banner2.png" />
-
         <SliderItem src="/images/banner3.png" />
-
         <SliderItem src="/images/banner4.png" />
       </StyledSlider>
     </Wrapper>
--- src/components/commons/layout/banner/LayoutBanner.styles.ts
+++ src/components/commons/layout/banner/LayoutBanner.styles.ts
@@ -3,16 +3,14 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-export const Wrapper = styled.div`
-`;
+export const Wrapper = styled.div``;
 
 export const SliderItem = styled.img`
   margin: auto;
 `;
 
 export const StyledSlider = styled(Slider)`
-	position: relative;
+  position: relative;
   .slick-prev::before,
   .slick-next::before {
     opacity: 0;
@@ -21,5 +19,24 @@ export const StyledSlider = styled(Slider)`
 `;
 
 export const ArrowImage = styled.img`
-	width: 40px;
-`
+  width: 30px;
+`;
+
+export const Next = styled.div`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  right: 30px;
+  z-index: 2;
+  text-align: right;
+  line-height: 30px;
+`;
+export const Prev = styled.div`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: 30px;
+  z-index: 2;
+  text-align: left;
+  line-height: 30px;
+`;
