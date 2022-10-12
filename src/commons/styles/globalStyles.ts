237c92bb646bf8379c2739f1f7eb3fbfae9b8782
ssalpyo <tjdvy6@gmail.com>
2022-10-13 01:51:19 +0900

    # Fix: 글로벌 스타일 수정

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -2,11 +2,30 @@ import { css } from "@emotion/react";
 
 export const globalStyles = css`
   * {
-    margin: 0;
-    box-sizing: border-box;
     font-family: "NotoSansKR";
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
-
+  button {
+    cursor: pointer;
+    border: none;
+    background-color: white;
+  }
+  a {
+    outline: none;
+    color: inherit;
+    text-decoration: none;
+  }
+  html {
+    font-size: 16px;
+  }
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    scroll-behavior: smooth;
+  }
   @font-face {
     font-family: "NotoSansKR";
     src: url("/fonts/NotoSansKR-Regular.otf");
