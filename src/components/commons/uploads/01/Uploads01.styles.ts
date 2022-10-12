c0025f38002c1afd102d11b697d0f44346b509fb
ssalpyo <tjdvy6@gmail.com>
2022-10-13 01:55:14 +0900

    # Fix: 이미지 업로드 UI 수정

--- src/components/commons/uploads/01/Uploads01.styles.ts
+++ src/components/commons/uploads/01/Uploads01.styles.ts
@@ -3,14 +3,14 @@ import styled from "@emotion/styled";
 export const UploadImage = styled.img`
   width: 78px;
   height: 78px;
-  margin-right: 24px;
+  margin-right: 14px;
 `;
 
 export const UploadButton = styled.button`
   width: 78px;
   height: 78px;
   background-color: #bdbdbd;
-  margin-right: 24px;
+  margin-right: 14px;
   outline: none;
   border: none;
   cursor: pointer;
