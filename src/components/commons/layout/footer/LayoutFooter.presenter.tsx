582e8265f915a208886556193ae3fad834c5dbd4
ssalpyo <tjdvy6@gmail.com>
2022-10-13 01:54:14 +0900

    # Add: 레이아웃 푸터 추가

--- src/components/commons/layout/footer/LayoutFooter.presenter.tsx
+++ src/components/commons/layout/footer/LayoutFooter.presenter.tsx
@@ -1,5 +1,26 @@
+import { CF } from "../../../../commons/styles/commonComponentStyle";
 import * as S from "./LayoutFooter.styles";
 
 export default function LayoutFooterUI() {
-  return <S.Wrapper></S.Wrapper>;
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <CF.ColumnDiv width="100%" gap={7} style={{ justifyContent: "center" }}>
+          <S.Title>ssalpyo</S.Title>
+          <S.CompanyText>㈜ssalpyo | 사업자 번호: 123-45-67890</S.CompanyText>
+          <S.CompanyText>통신판매업신고번호 : 2022-서울-1111</S.CompanyText>
+          <S.CompanyText>대표 : 홍성표</S.CompanyText>
+          <S.CompanyText>서울특별시 영등포구 여의도동</S.CompanyText>
+        </CF.ColumnDiv>
+        <CF.ColumnDiv width="100%" style={{ justifyContent: "center" }}>
+          <S.Title>고객센터</S.Title>
+          <S.InfoText>이메일 : tjdvy6@gmail.com</S.InfoText>
+          <S.InfoText>고객센터 : 010-9215-6673</S.InfoText>
+          <S.InfoText>
+            업무시간 : 평일 10:00 - 17:00 (점심: 12:00 - 13:00)
+          </S.InfoText>
+        </CF.ColumnDiv>
+      </S.Wrapper>
+    </S.Container>
+  );
 }
--- src/components/commons/layout/footer/LayoutFooter.styles.ts
+++ src/components/commons/layout/footer/LayoutFooter.styles.ts
@@ -1,7 +1,57 @@
+import theme from "../../../../commons/styles/theme";
 import styled from "@emotion/styled";
+
+export const Container = styled.footer`
+  margin: auto;
+  width: 100%;
+  background-color: #999;
+  padding: 60px 0;
+
+  @media ${theme.device.tablet} {
+    padding-bottom: 80px;
+  }
+`;
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 285px;
-  background-color: #999;
+  max-width: 1024px;
+  padding: 0 20px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  gap: 100px;
+`;
+
+export const Title = styled.h2`
+  ${theme.fontSizes.subTitle};
+  font-family: sans-serif;
+  color: rgb(51, 51, 51);
+`;
+
+export const InfoText = styled.p`
+  margin: 0;
+  margin-bottom: 10px;
+  color: rgb(51, 51, 51);
+  ${theme.fontSizes.base};
+  font-weight: 500;
+  font-family: sans-serif;
+  :last-child {
+    margin-bottom: 0;
+  }
+
+  @media ${theme.device.tablet} {
+    ${theme.fontSizes.small};
+  }
+`;
+
+export const CompanyText = styled.p`
+  margin: 0;
+  margin-bottom: 10px;
+  color: rgb(51, 51, 51);
+  ${theme.fontSizes.small};
+  font-weight: 500;
+  font-family: sans-serif;
+  :last-child {
+    margin-bottom: 0;
+  }
 `;
