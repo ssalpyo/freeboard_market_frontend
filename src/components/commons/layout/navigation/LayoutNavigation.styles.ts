51585779e67dd432f4009d245546428747ddc38a
ssalpyo <tjdvy6@gmail.com>
2022-10-18 00:51:03 +0900

    # Update: 레이아웃 네비게이션 반응형 작업

--- src/components/commons/layout/navigation/LayoutNavigation.styles.ts
+++ src/components/commons/layout/navigation/LayoutNavigation.styles.ts
@@ -1,23 +1,32 @@
 import styled from "@emotion/styled";
+import theme from "../../../../commons/styles/theme";
 
 export const Wrapper = styled.div`
-  height: 64px;
+  width: 100%;
+  height: 60px;
   background-color: #999;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  font-size: 18px;
-  color: white;
 `;
 
 export const MenuItem = styled.div`
-  margin: 0px 60px;
-  /* color: #656565; */
+  margin: 0 60px;
+  font-size: 18px;
   color: #fff;
   cursor: pointer;
 
   :hover {
     color: #000;
+  }
+
+  @media ${theme.device.tablet} {
+    margin: 0 20px;
+    font-size: 14px;
+  }
+  @media ${theme.device.mobile} {
+    margin: 0 10px;
+    font-size: 12px;
   }
 `;
