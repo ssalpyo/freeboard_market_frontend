94249050ad0cd72fc51b68f1cca3203e592f464c
ssalpyo <tjdvy6@gmail.com>
2022-10-23 21:12:08 +0900

    # Add: 로그인, 회원가입 인풋 공통 컴포넌트

--- src/components/commons/inputs/sign-in-up/index.tsx
+++ src/components/commons/inputs/sign-in-up/index.tsx
@@ -1,22 +1,20 @@
 import styled from "@emotion/styled";
 
 export const Input = styled.input`
-  border: 1px solid #ffffff;
-  border-radius: 16px;
-  padding: 0px 16px;
-  width: 384px;
-  height: 64px;
-  margin-top: 15px;
-  margin-bottom: 4px;
-  background: none;
+  width: 100%;
+  max-width: 780px;
+  height: 60px;
+  background: #f6f6f6;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 0px 20px;
+  color: #6b6b6b;
   outline-color: #000;
-  font-family: "Noto Sans CJK KR";
+  font-family: "Noto Sans";
   font-style: normal;
   font-weight: 400;
-  font-size: 20px;
-  line-height: 24px;
-  color: #fff;
-  opacity: 0.6;
+  font-size: 16px;
+  line-height: 100%;
 `;
 
 interface IProps {
