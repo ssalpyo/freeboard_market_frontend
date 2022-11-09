1748a318c5ac1739e4ce0d0ce2f9718100086494
ssalpyo <tjdvy6@gmail.com>
2022-11-10 00:10:04 +0900

    마이페이지 테스트 주석처리

--- src/components/units/mypage/MyPage.presenter.tsx
+++ src/components/units/mypage/MyPage.presenter.tsx
@@ -1,25 +1,21 @@
-import styled from '@emotion/styled'
+// import styled from "@emotion/styled";
 
-const Test = styled.div`
-	width: 300px;
-	height: 300px;
-	background-color: skyblue;
-	animation: fadeIn 3s forwards;
+// const Test = styled.div`
+//   width: 300px;
+//   height: 300px;
+//   background-color: skyblue;
+//   animation: fadeIn 3s forwards;
 
-@keyframes fadeIn {
-  from {
-    width: 300px;
-  }
-  to {
-    width: 600px;
-  }
-}
-`
+//   @keyframes fadeIn {
+//     from {
+//       width: 300px;
+//     }
+//     to {
+//       width: 600px;
+//     }
+//   }
+// `;
 
 export default function MyPageUI() {
-  return (
-		<div>
-			<Test></Test>
-		</div>
-	)
+  return <div>{/* <Test></Test> */}</div>;
 }
